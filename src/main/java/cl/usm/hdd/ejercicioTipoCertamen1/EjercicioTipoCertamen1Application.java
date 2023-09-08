57137f6ee2dfb9bb4ef43f5c1d6ed2c858ba0480
Jorge Vicuña <jvicuna2001@gmail.com>
2023-09-08 15:08:50 -0300

    Base proyecto

---
+++ src/main/java/cl/usm/hdd/ejercicioTipoCertamen1/EjercicioTipoCertamen1Application.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.ejercicioTipoCertamen1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioTipoCertamen1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioTipoCertamen1Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/usm/hdd/ejercicioTipoCertamen1/EjercicioTipoCertamen1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.ejercicioTipoCertamen1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioTipoCertamen1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
